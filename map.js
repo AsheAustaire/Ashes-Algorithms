a52a67e49142eae95087ff4acf799ee5d6bf1195
Ashe Austaire <AsheAustaire@gmail.com>
2018-01-19 15:11:01 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,10 +1,22 @@
 //BASIC MAP FUNCTION
 
+
+//Way 1
 function map(arr, func) {
   let i;
   newArr = [...arr]
   for(i = 0; i < arr.length; i++) {
     newArr[i] = func(newArr[i])
+  }
+  return newArr
+}
+
+//Way 2
+function map(arr, func) {
+  let i;
+  newArr = []
+  for(i = 0; i < arr.length; i++) {
+    newArr.push(func(arr[i]))
   }
   return newArr
 }
